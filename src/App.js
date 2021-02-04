6c8b42858095b997bd428722a1a9cb7f5afa6383
Vanessa Martin <vanessa.s.martin96@gmail.com>
2021-02-04 11:25:30 -0800

    setting up pricing page files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/common/navigation/Nav";
 import Home from "./pages/home/Home";
 import Stories from "./pages/stories/Stories";
 import Features from "./pages/features/Features";
+import Pricing from "./pages/pricing/Pricing";
 import Footer from "./components/common/footer/Footer";
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/stories" component={Stories} />
-        <Route exact path="/features" component={Features} />
+        X<Route exact path="/features" component={Features} />
+        X<Route exact path="/pricing" component={Pricing} />
       </Switch>
       <Footer />
     </div>
